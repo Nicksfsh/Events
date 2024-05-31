5e343c1890865b930c78268c6ba34d03fa15d2b0
Nicksfsh <yny007@gmail.com>
2024-05-31 07:40:12 +0300

    Initial commit

---
+++ models/Registration.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const registrationSchema = new mongoose.Schema({
+    eventId: String,
+    fullName: String,
+    email: String,
+    dateOfBirth: Date,
+    startDate: Date,
+    source: String
+});
+
+module.exports = mongoose.model('Registration', registrationSchema);
+
